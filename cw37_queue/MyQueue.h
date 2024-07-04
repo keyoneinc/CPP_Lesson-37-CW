3fe8a1164b6f1734e934c5c97d66d69303de8288
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-04 19:55:49 +0300

    added MyQueue and it's definition

---
+++ cw37_queue/MyQueue.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+using std::cout;
+using std::endl;
+
+namespace MyQueue {
+	template <typename T>
+	class Node {
+	public:
+		T info;
+		Node* next;
+		Node* prev;
+
+		Node(T value) {
+			info = value;
+			next = prev = nullptr;
+		}
+	};
+
+	//FIFO = First In First Out
+	template <typename U>
+	class Queue {
+	protected:
+		Node<U>* head;
+		Node<U>* tail;
+	public:
+		Queue() {
+			head = tail = nullptr;
+		}
+		~Queue() {
+			//реалізація
+		}
+
+		void pop_front() {//First Out
+
+		}
+		void push_back() {
+
+		}
+		U first() const {// First element
+			if (isEmpty()) throw "Queue is empty.";
+
+			return head->info;
+		}
+		bool isEmpty()const {
+			return head == nullptr || tail == nullptr;
+		}
+		void showInfo()const {
+			if (isEmpty()) {
+				cout << "Queue is empty.\n";
+				return;
+			}
+
+			Node<U>* p = head;
+			while (p != nullptr) {
+				cout << p->info << " ";
+				p = p->next;
+			}
+			cout << endl;
+		}
+	};
+
+}
